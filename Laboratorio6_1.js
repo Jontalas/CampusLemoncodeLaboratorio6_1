984b0b028a908c42186b63d787078dc094d60de3
Jonatan <jontalas@gmail.com>
2021-12-28 03:50:46 +0100

    Entrega

--- Laboratorio6_1.js
+++ Laboratorio6_1.js
@@ -4,7 +4,7 @@ const LOWER_TYPE = 4;
 const EXEMPT_TYPE = 0;
 
 // Entrada.
-const products = [
+const productList = [
     {
         description: "Goma de borrar",
         price: 0.25,
@@ -62,60 +62,83 @@ const products = [
         units: 0,
     },
 ];
-var showProductList = () => {
+var showProducts = () => {
     var main = document.getElementById("main");
-    for (var i in products) {
-        const product = products[i];
-        const productRow = document.createElement("div");
-        productRow.setAttribute("class", "element");
-        main.appendChild(productRow);
-        const productRowLeft = document.createElement("div");
-        productRowLeft.setAttribute("class", "element-left");
-        productRowLeft.setAttribute("align", "left");
-        productRow.appendChild(productRowLeft);
-        const productRowRight = document.createElement("div");
-        productRowRight.setAttribute("class", "element-right");
-        productRowRight.setAttribute("align", "right");
-        productRow.appendChild(productRowRight);
-        const productOrder = document.createElement("label");
-        productOrder.textContent = Number(i) + 1 + ".";
-        productRowLeft.appendChild(productOrder);
-        const productDescription = document.createElement("label");
-        productDescription.setAttribute("for", "productCount" + i);
-        productDescription.textContent = product.description + " - " + Number(product.price).toFixed(2) + "€/ud. ";
-        productRowRight.appendChild(productDescription);
-        const productCount = document.createElement("input");
-        productCount.setAttribute("class", "product-count");
-        productCount.id = "productCount" + i;
-        productCount.type = "number";
-        productCount.value = 0;
-        productCount.addEventListener("change", event => HandleChangeCount(event));
-        productRowRight.appendChild(productCount);
+    for (var i in productList) {
+        const product = productList[i];
+        const productRow = createProductRow(main);
+        const productRowLeft = createProductRowLeft(productRow);
+        const productRowRight = createProductRowRight(productRow);
+        const productOrder = createProductOrder(i,productRowLeft);
+        const productDescription = createProductDescription(product,productRowRight);
+        const productCount = createProductCount(product,productRowRight);
     }
 }
-var HandleChangeCount = event => {
-    const i = Number(event.target.id.substring(12));
+var createProductRow = (container) => {
+    const productRow = document.createElement("div");
+    productRow.setAttribute("class", "element");
+    container.appendChild(productRow);
+    return productRow;
+}
+var createProductRowLeft = (container) => {
+    const productRowLeft = document.createElement("div");
+    productRowLeft.setAttribute("class", "element-left");
+    productRowLeft.setAttribute("align", "left");
+    container.appendChild(productRowLeft);
+    return productRowLeft;
+}
+var createProductRowRight = (container) => {
+    const productRowRight = document.createElement("div");
+    productRowRight.setAttribute("class", "element-right");
+    productRowRight.setAttribute("align", "right");
+    container.appendChild(productRowRight);
+    return productRowRight;
+}
+var createProductOrder = (i,container) => {
+    const productOrder = document.createElement("label");
+    productOrder.textContent = Number(i) + 1 + ".";
+    container.appendChild(productOrder);
+    return productOrder;
+}
+var createProductDescription = (product,container) => {
+    const productDescription = document.createElement("label");
+    productDescription.textContent = product.description + " - " + Number(product.price).toFixed(2) + "€/ud. ";
+    container.appendChild(productDescription);
+    return productDescription;
+}
+var createProductCount = (product,container) => {
+    const productCount = document.createElement("input");
+    productCount.setAttribute("class", "product-count");
+    productCount.type = "number";
+    productCount.value = 0;
+    productCount.min = 0;
+    productCount.max = product.stock
+    productCount.addEventListener("change", event => HandleChangeCount(event,product));
+    container.appendChild(productCount);
+    return productCount;
+}
+var HandleChangeCount = (event,product) => {
     if (Number(event.target.value) < 0) {
         event.target.value = 0;
     };
-    if (Number(event.target.value) > products[i].stock) {
-        event.target.value = products[i].stock;
+    if (Number(event.target.value) > product.stock) {
+        event.target.value = product.stock;
     };
-    products[i].units = Number(event.target.value);
+    product.units = event.target.value;
     var enableCalculate = false;
     var countIterator = 0;
-    while (countIterator < products.length && !enableCalculate) {
-        enableCalculate = products[countIterator++].units > 0;
+    while (countIterator < productList.length && !enableCalculate) {
+        enableCalculate = productList[countIterator++].units > 0;
     };
     document.getElementById("calculateButton").disabled = !enableCalculate;
 }
 var calculate = () => {
     var subtotal = 0;
     var taxes = 0;
-    for (i in products) {
-        productCalculation = products[i].units * products[i].price;
+    for (i in productList) {
+        productCalculation = productList[i].units * productList[i].price;
         subtotal += productCalculation;
-        switch (products[i].tax) {
+        switch (productList[i].tax) {
             case REGULAR_TYPE:
                 taxes += productCalculation * .21;
                 break;
@@ -130,4 +153,4 @@ var calculate = () => {
     document.getElementById("total").innerHTML = "Total: " + Number(total).toFixed(2) + " €";
 }
 // ------------- MAIN -------------
-showProductList();
+showProducts();
